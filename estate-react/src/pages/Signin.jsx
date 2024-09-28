33d6b63e3a1d09bd449591e41527e74797708836
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-09-28 20:45:12 +0300

    30

--- estate-react/src/pages/Signin.jsx
+++ estate-react/src/pages/Signin.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Signin = () => {
+  const [loading, setloading] = useState(false);
+  const [error, seterror] = useState(null);
+  const [FormData, setformdata] = useState({});
+
+  const handleinput = (e) => {
+    setformdata({
+      ...FormData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setloading(true);
+      const response = await fetch("http://localhost:3000/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(FormData),
+      });
+      const data = response.json();
+      if (!response.ok) {
+        const message = `Error: ${response.status} ${response.statusText}`;
+        seterror(message); // Set error message from the response
+        setloading(false);
+        return;
+      }
+      console.log(data);
+
+      setloading(false);
+      seterror(null);
+    } catch (error) {
+      setloading(false);
+      seterror(error);
+    }
+  };
+
   return (
     <div className="flex justify-center">
       <form
@@ -13,19 +52,30 @@ export const Signin = () => {
           type="email"
           placeholder="email"
           className=" p-3 rounded-lg border"
+          onChange={handleinput}
+          id="email"
         />
         <input
           type="password"
           placeholder="password"
           className=" p-3 rounded-lg border"
+          onChange={handleinput}
+          id="password"
         />
-        <button className="bg-white rounded-lg p-3">signin</button>
+        <button
+          disabled={loading}
+          className="bg-black text-white rounded-lg p-3 disabled:bg-red-900"
+          onClick={handlesubmit}
+        >
+          {loading ? "loading....." : "signin"}
+        </button>
         <div>
           have no an account ?{" "}
           <Link to={"/signup"} className="bg-blue-400 rounded-lg p-3">
             Sign up here{" "}
           </Link>
         </div>
+        {error && <p className="text-red-800 font-semibold">{error}!</p>}
       </form>
     </div>
   );
